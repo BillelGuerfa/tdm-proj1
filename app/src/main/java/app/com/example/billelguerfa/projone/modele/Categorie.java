6e98cd9b16bc247f80ac28ca6ed57ee46ef2dfce
BillelGuerfa <cb_guerfa@esi.dz>
2016-04-04 13:51:53 +0100

    add classes Produit , Categorie, Commande

--- app/src/main/java/app/com/example/billelguerfa/projone/modele/Categorie.java
+++ app/src/main/java/app/com/example/billelguerfa/projone/modele/Categorie.java
@@ -1,7 +1,34 @@
 package app.com.example.billelguerfa.projone.modele;
 
+import java.io.Serializable;
+import java.util.List;
+
 /**
  * Created by Billel Guerfa on 04/04/2016.
  */
-public class Categorie {
+public class Categorie implements Serializable{
+    boolean racine;
+
+    public Categorie(boolean racine, List<Categorie> sousCategories) {
+        this.racine = racine;
+        this.sousCategories = sousCategories;
+    }
+
+    public boolean isRacine() {
+        return racine;
+    }
+
+    public void setRacine(boolean racine) {
+        this.racine = racine;
+    }
+
+    public List<Categorie> getSousCategories() {
+        return sousCategories;
+    }
+
+    public void setSousCategories(List<Categorie> sousCategories) {
+        this.sousCategories = sousCategories;
+    }
+
+    private List<Categorie> sousCategories;
 }
---
+++ app/src/main/java/app/com/example/billelguerfa/projone/modele/Commande.java
@@ -0,0 +1,24 @@
+package app.com.example.billelguerfa.projone.modele;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Billel Guerfa on 04/04/2016.
+ */
+public class Commande {
+    private List<Produit> produits;
+
+    public Commande() {
+        this.produits = new ArrayList<Produit>();
+    }
+
+    public List<Produit> getProduits() {
+
+        return produits;
+    }
+
+    public void setProduits(List<Produit> produits) {
+        this.produits = produits;
+    }
+}
